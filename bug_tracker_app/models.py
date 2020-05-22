d0dcadc86b9b40671d9e3c0e85350db1abce2fa9
Mustafa Prodhan <mustafa.prodhan.317@gmail.com>
2020-05-22 17:47:03 -0400

    added models.py with fields to be stored and referenced.

--- bug_tracker_app/models.py
+++ bug_tracker_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class BugTicket(models.Model):
+    number = models.AutoField(primary_key=True)
+    titles = models.CharField(max_length=60)
+    assigned = models.CharField(max_length=30)
+    reported = models.CharField(max_length=30)
+    ticket_age = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.number} - {self.titles}"
+
+
+class BugSubmit(models.Model):
+    title = models.CharField(max_length=60)
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
